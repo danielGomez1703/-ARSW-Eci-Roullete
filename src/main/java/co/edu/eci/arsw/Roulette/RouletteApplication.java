723b0d62567f5640ea829331e3ed7b4b59320de3
Daniel Gomez <danipipe1703@gmail.com>
2020-06-23 01:13:39 -0500

    construccions spring

---
+++ src/main/java/co/edu/eci/arsw/Roulette/RouletteApplication.java
@@ -0,0 +1,13 @@
+package co.edu.eci.arsw.Roulette;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RouletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RouletteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/eci/arsw/Roulette/RouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.eci.arsw.Roulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
