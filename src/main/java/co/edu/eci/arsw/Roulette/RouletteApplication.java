8b4340d6ed71f94a4cfd6e496e5456f87c250747
Daniel Gomez <danipipe1703@gmail.com>
2020-06-28 01:14:14 -0500

    vista login and sign up

--- src/main/java/co/edu/eci/arsw/Roulette/RouletteApplication.java
+++ src/main/java/co/edu/eci/arsw/Roulette/RouletteApplication.java
@@ -2,8 +2,10 @@ package co.edu.eci.arsw.Roulette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RouletteApplication {
 
 	public static void main(String[] args) {
